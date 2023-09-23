7a470c5f98cc3968480c2879be741e936253268c
Konstantin Mindlin <MindlinKS@yandex.ru>
2023-09-23 18:51:16 +0300

    add celery.py

--- backend/files/models.py
+++ backend/files/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField(
+        'Файл',
+        upload_to='uploaded_files/'
+    )
+    uploaded_at = models.DateTimeField(
+        'Дата и время загрузки',
+        auto_now_add=True
+    )
+    processed = models.BooleanField(
+        'Обработан',
+        default=False
+    )
