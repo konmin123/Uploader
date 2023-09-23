ac87ee9e607f3ffd92e29de82a3c20ed707e885b
Konstantin Mindlin <MindlinKS@yandex.ru>
2023-09-23 21:42:41 +0300

    add celery.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,2 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import FileViewSet
+from .views import UploadViewSet
+
+router = routers.DefaultRouter()
+router.register('upload', UploadViewSet)
+router.register('files', FileViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from files.models import File
+from .serializers import FileSerializer
+
+
+class UploadViewSet(mixins.CreateModelMixin, GenericViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    http_method_names = ['post']
+
+    def perform_create(self, serializer):
+        # запуск ассинхронной задачи обработки через celary
+        serializer.save()
+
+
+class FileViewSet(ReadOnlyModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
